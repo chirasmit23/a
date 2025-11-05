ba135a9f25ee78f535c2fbd06f8bed31e7848cbb
chirasmit23 <149405361+chirasmit23@users.noreply.github.com>
2025-11-05 11:59:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,9 @@ from langchain_core.documents import Document
 from langchain_core.prompts import ChatPromptTemplate
 
 # ✅ Chains (these are now inside langchain.chains)
-from langchain.chains.combine_document_chain import create_stuff_documents_chain
-from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+
 
 
 # ✅ Chat Models (external providers)
